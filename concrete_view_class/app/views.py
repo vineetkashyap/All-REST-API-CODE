86e6accdeabe7b4d46d280636e968364034dcd64
vineetkashyap <vkvineet66@gmail.com>
2021-03-18 16:28:50 +0530

    concrete view class

--- concrete_view_class/app/views.py
+++ concrete_view_class/app/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializers
-from rest_framework.generics import  ListCreateAPIView,RetrieveUpdateDestroyAPIView,ListAPIView,CreateAPIView,RetrieveAPIView,DestroyAPIView,UpdateAPIView
+from rest_framework.generics import  ListCreateAPIView,RetrieveUpdateDestroyAPIView,ListAPIView,CreateAPIView,RetrieveAPIView,DestroyAPIView,UpdateAPIView,RetrieveUpdateAPIView,RetrieveDestroyAPIView
 
 class MyList(ListAPIView):
     queryset=Student.objects.all()
     serializer_class= StudentSerializers
+
 class MyList1(CreateAPIView):
     queryset=Student.objects.all()
     serializer_class= StudentSerializers
+
 class MyList2(RetrieveAPIView):
     queryset=Student.objects.all()
     serializer_class= StudentSerializers
+
 class MyList3(UpdateAPIView):
     queryset=Student.objects.all()
     serializer_class= StudentSerializers
+
 class MyList4(DestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class= StudentSerializers
+
+class MyList5(ListCreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class= StudentSerializers
+
+class MyList6(RetrieveUpdateDestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class= StudentSerializers
+
+class MyList7(RetrieveUpdateAPIView):
+    queryset=Student.objects.all()
+    serializer_class= StudentSerializers
+
+class MyList8(RetrieveDestroyAPIView):
     queryset=Student.objects.all()
     serializer_class= StudentSerializers
 
--- concrete_view_class/concrete_view_class/urls.py
+++ concrete_view_class/concrete_view_class/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('stu2/<int:pk>',views.MyList2.as_view()),
     path('stu3/<int:pk>',views.MyList3.as_view()),
     path('stu4/<int:pk>',views.MyList4.as_view()),
+    path('stu5/',views.MyList5.as_view()),
+    path('stu6/<int:pk>',views.MyList6.as_view()),
+    path('stu7/<int:pk>',views.MyList7.as_view()),
 ]
